ae891952f3ac2b23eec34055f2f64cdba197abd9
arielzn <arielzn@gmail.com>
2021-06-28 11:42:03 +0200

    add pre-assemble script

---
+++ php-pre-assemble/99-elabftw-pre-assemble.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#add new zopfli build requirement
+
+dnf install --allowerasing -y https://archives.fedoraproject.org/pub/archive/fedora/linux/releases/29/Everything/x86_64/os/Packages/z/zopfli-1.0.1-7.fc29.x86_64.rpm
